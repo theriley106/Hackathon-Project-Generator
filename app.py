dc0ffd23e5d6aaccbe76b44f77545453c0ea778a
theriley106 <christopherlambert106@gmail.com>
2020-03-07 00:01:57 -0800

    Added endpoint to generate ideas

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, request, url_for, redirect, Markup, jsonify, make_response, send_from_directory, session
+import random
 
 app = Flask(__name__, static_url_path='/static')
 
+@app.route("/newIdea", methods=["GET"])
+def get_new():
+	ideas = [x for x in open("ideas.txt").read().split("\n") if len(x) > 5]
+	return random.choice(ideas)
 
 @app.route('/', methods=['GET'])
 def index():
