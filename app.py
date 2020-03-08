94d6d987cad67893c42512e77a4c4552856dfe22
theriley106 <christopherlambert106@gmail.com>
2020-03-08 01:46:53 -0800

    Fixed app

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ hackathon_ideas = db.hackathon_ideas
 print("FINISHED HACKATHON IDEAS")
 
 # IDEAS = [x for x in open("ideas.txt").read().split("\n") if len(x) > 5 and len(x) < 200]
-IDEAS = [x for x in db.hackathon_ideas.find({})]
+IDEAS = [x for x in db.hackathon_ideas.find(limit=10)]
 print("FINISHED IDEAS")
 RECENT = []
 STEPS = ["Generating Project Name", "Generating Logo", "Buying Domain Name", "Raising Seed Round", "Building Starter Code", "Preparing Zip File"]
